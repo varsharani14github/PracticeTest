a48f0a3a2457d645d6d3f36f7d10dcad304809dd
Varsharani <varsharanigiramkar4@gmail.com>
2024-06-29 15:29:35 +0530

    even odd number code here

---
+++ Practice1/src/com/velocity/EvenNumber.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+
+		int a = 11;
+		if (a / 2 == 0) {
+			System.out.println("It's Even number");
+		} else {
+			System.out.println("It's Odd number");
+		}
+	}
+}
